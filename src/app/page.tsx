be40b00a888ecc16ce81da55ca2d98ad08f17c56
aay6ush <aay6ush@gmail.com>
2024-09-24 00:52:14 +0530

    feat: add light/dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         lets fkin build this shit!
       </h1>
       <Button>click me</Button>
+      <ModeToggle />
     </main>
   );
 }
